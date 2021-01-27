e3f29bba48856fca9f85b7c97f73effdbd2c84ed
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-01-27 19:05:16 +0530

    Added other grant types

--- authorization-server/src/main/java/com/javamultiplex/config/AuthServerConfig.java
+++ authorization-server/src/main/java/com/javamultiplex/config/AuthServerConfig.java
@@ -3,6 +3,7 @@ package com.javamultiplex.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,23 +20,33 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("client1")
                 .secret("secret1")
                 .scopes("read")
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
                 .and()
                 .withClient("client2")
                 .secret("secret2")
                 .scopes("read")
                 .authorizedGrantTypes("authorization_code")
-                .redirectUris("http://localhost:9090");
+                .redirectUris("http://localhost:9090")
+                .and()
+                .withClient("client3")
+                .secret("secret3")
+                .scopes("read")
+                .authorizedGrantTypes("client_credentials");
+
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
